a7f8831eb331b344f08247c4b6c4984b08576728
Gabriela França <gabifmouraroc@outlook.com>
2024-02-07 16:10:50 -0300

    desabilitando botão

--- src/components/New-note-card.tsx
+++ src/components/New-note-card.tsx
@@ -78,8 +78,9 @@ export function NewNoteCard() {
             </div>
 
             <button
+              disabled={!content}
               type="submit"
-              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:enabled:bg-lime-500"
             >
               Salvar nota
             </button>
