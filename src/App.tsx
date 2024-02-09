a7c7cc9f7405e13907eac3c210087ad0bc76ce0e
Gabriela França <gabifmouraroc@outlook.com>
2024-02-09 11:17:00 -0300

    aplicando responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export function App() {
     : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="logo nlw expert" />
       <form className="w-full">
         <input
@@ -35,7 +35,7 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6  auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  auto-rows-[250px]">
         <NewNoteCard />
 
         {showNotes.map((item) => (
