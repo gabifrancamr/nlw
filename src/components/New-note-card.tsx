0b1b39759fdd8d69cf4064255377a4e0effe5ba7
Gabriela França <gabifmouraroc@outlook.com>
2024-02-09 12:33:19 -0300

    botão de retorno ao onboarding

--- src/components/New-note-card.tsx
+++ src/components/New-note-card.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'lucide-react'
+import { ArrowLeft, X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useContext, useState } from 'react'
 import { toast } from 'sonner'
 import { NotesContext } from '../context/NotesContext'
@@ -84,6 +84,17 @@ export function NewNoteCard() {
     speecheRecognition?.stop()
   }
 
+  function buttonBack() {
+    if (isRecording) {
+      speecheRecognition?.stop()
+      setIsRecording(false)
+    }
+
+    setContent('')
+
+    setShouldShowOnboarding(true)
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -99,9 +110,19 @@ export function NewNoteCard() {
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
         <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
-          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
-            <X className="size-5" />
-          </Dialog.Close>
+          <div className="flex cursor-pointer mb-6">
+            {!shouldShowOnboarding && (
+              <button
+                onClick={buttonBack}
+                className="absolute left-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100"
+              >
+                <ArrowLeft />
+              </button>
+            )}
+            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+              <X className="size-5" />
+            </Dialog.Close>
+          </div>
           <form className="flex-1 flex flex-col">
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-sm font-medium text-slate-300">
@@ -137,7 +158,7 @@ export function NewNoteCard() {
               )}
             </div>
 
-            {isRecording ? (
+            {isRecording && shouldShowOnboarding === false ? (
               <button
                 type="button"
                 onClick={handleStopRecording}
