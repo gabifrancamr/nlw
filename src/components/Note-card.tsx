aa982b72824dd7ed2d1f960e0ae70f013a7bb2b7
Gabriela França <gabifmouraroc@outlook.com>
2024-02-09 13:35:40 -0300

    colocando função no botão

--- src/components/Note-card.tsx
+++ src/components/Note-card.tsx
@@ -53,14 +53,12 @@ export function NoteCard({ note }: NoteCardProps) {
           </div>
 
           <button
+            onClick={handleDeleteNote}
             type="button"
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
             Deseja{' '}
-            <span
-              onClick={handleDeleteNote}
-              className="text-red-400 group-hover:underline"
-            >
+            <span className="text-red-400 group-hover:underline">
               apagar essa nota
             </span>
             ?
